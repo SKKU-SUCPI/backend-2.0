8ec2985bb38678bfb109490d240a93869bf2178a
Jinjeon Shin <sjg729729@gmail.com>
2025-01-30 16:49:35 +0900

    Initial commit

---
+++ src/main/java/com/skku/sucpi/SucpiApplication.java
@@ -0,0 +1,13 @@
+package com.skku.sucpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skku/sucpi/SucpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skku.sucpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
