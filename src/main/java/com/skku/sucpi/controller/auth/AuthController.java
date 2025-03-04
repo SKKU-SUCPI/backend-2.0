7a8ba08c31c4c198cc530fb2898ebed57665dd48
신진건 <sjg729729@gmail.com>
2025-03-04 17:21:50 +0900

    [feat]sucpi#5 - 권한별 로그인 추가

--- src/main/java/com/skku/sucpi/controller/auth/AuthController.java
+++ src/main/java/com/skku/sucpi/controller/auth/AuthController.java
@@ -79,11 +79,106 @@ public class AuthController {
         return ResponseEntity.ok("로그인 성공");
     }
 
+    // test api
+    @PostMapping("/login/student")
+    public ResponseEntity<String> loginStudent(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        SSOUserDto ssoUserDto = SSOUserDto
+                .builder()
+                .userName("신진건")
+                .hakbun("2020310328")
+                .role("student")
+                .build();
+
+//        User user = userService.getOrCreateUser(ssoUserDto);
+//
+//        String accessToken = jwtUtil.generateAccessToken(user.getName(), user.getId(), user.getRole());
+//        String refreshToken = jwtUtil.generateRefreshToken(user.getName());
+
+        String accessToken = jwtUtil.generateAccessToken("신진건", 2020310328L, "student");
+        String refreshToken = jwtUtil.generateRefreshToken("신진건");
+
+
+        addCookie(response, "accessToken", accessToken, 15 * 60);  // 15분
+        addCookie(response, "refreshToken", refreshToken, 7 * 24 * 60 * 60);  // 7일
+
+
+        return ResponseEntity.ok("student login success");
+    }
+
+    // test api
+    @PostMapping("/login/admin")
+    public ResponseEntity<String> loginAdmin(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        SSOUserDto ssoUserDto = SSOUserDto
+                .builder()
+                .userName("Admin")
+                .hakbun("11111111")
+                .role("admin")
+                .build();
+
+        User user = userService.getOrCreateUser(ssoUserDto);
+
+        String accessToken = jwtUtil.generateAccessToken(user.getName(), user.getId(), user.getRole());
+        String refreshToken = jwtUtil.generateRefreshToken(user.getName());
+
+
+        addCookie(response, "accessToken", accessToken, 15 * 60);  // 15분
+        addCookie(response, "refreshToken", refreshToken, 7 * 24 * 60 * 60);  // 7일
+
+
+        return ResponseEntity.ok("admin login success");
+    }
+
+    // test api
+    @PostMapping("/login/super-admin")
+    public ResponseEntity<String> loginSuperAdmin(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        SSOUserDto ssoUserDto = SSOUserDto
+                .builder()
+                .userName("SuperAdmin")
+                .hakbun("00000000")
+                .role("super-admin")
+                .build();
+
+        User user = userService.getOrCreateUser(ssoUserDto);
+
+        String accessToken = jwtUtil.generateAccessToken(user.getName(), user.getId(), user.getRole());
+        String refreshToken = jwtUtil.generateRefreshToken(user.getName());
+
+
+        addCookie(response, "accessToken", accessToken, 15 * 60);  // 15분
+        addCookie(response, "refreshToken", refreshToken, 7 * 24 * 60 * 60);  // 7일
+
+
+        return ResponseEntity.ok("super-admin login success");
+    }
+
     // 검증용 api
     @PostMapping("/student")
     @PreAuthorize("hasRole('student')")
     public String checkStudent() {
         return "학생입니다.";
+    }
+
+    // 검증용 api
+    @PostMapping("/admin")
+    @PreAuthorize("hasRole('admin')")
+    public String checkAdmin() {
+        return "admin 입니다.";
+    }
+
+    // 검증용 api
+    @PostMapping("/super-admin")
+    @PreAuthorize("hasRole('super-admin')")
+    public String checkSuperAdmin() {
+        return "super-admin 입니다.";
     }
 
     private void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
