e78fe99e20b1b7c835dd195a7b625d1269dc04d1
신진건 <sjg729729@gmail.com>
2025-09-09 14:24:48 +0900

    [Fix] API - test 관련 코드 삭제

--- src/main/java/com/skku/sucpi/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.skku.sucpi.repository;
-
-import com.skku.sucpi.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
